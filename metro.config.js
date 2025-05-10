9746b3ddefa3567b9892c013e3afe9961d8bd2db
kvgharbigit <67672489+kvgharbigit@users.noreply.github.com>
2025-05-10 15:39:19 +1000

    github ubilder

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Add Node.js polyfills
+// Add Node.js polyfills and package shims
 config.resolver = {
   ...config.resolver,
   extraNodeModules: {
@@ -14,6 +15,29 @@ config.resolver = {
     'crypto': require.resolve('crypto-browserify'),
     'buffer': require.resolve('buffer'),
     'process': require.resolve('process/browser'),
+    'os': require.resolve('os-browserify'),
+    'vm': require.resolve('vm-browserify'),
+    'zlib': require.resolve('browserify-zlib'),
+    'assert': require.resolve('assert'),
+    'util': require.resolve('util'),
+    'path': require.resolve('path-browserify'),
+    'events': require.resolve('events'),
+    'constants': require.resolve('constants-browserify'),
+    'tty': require.resolve('tty-browserify'),
+    'url': require.resolve('url'),
+    'punycode': require.resolve('punycode'),
+    'querystring': require.resolve('querystring-es3'),
+    'string_decoder': require.resolve('string_decoder'),
+    'timers': require.resolve('timers-browserify'),
+    'domain': require.resolve('domain-browser'),
+    
+    // Custom shims for complex modules
+    'ws': path.resolve(__dirname, './ws-shim.js'),
+    'net': path.resolve(__dirname, './net-shim.js'),
+    'tls': path.resolve(__dirname, './tls-shim.js'),
+    'dns': path.resolve(__dirname, './dns-shim.js'),
+    'dgram': path.resolve(__dirname, './dgram-shim.js'),
+    'fs': path.resolve(__dirname, './fs-shim.js'),
   }
 };
 
--- node-polyfills.js
+++ node-polyfills.js
@@ -1,22 +1,51 @@
 // This file provides polyfills for Node.js core modules in React Native
+// Import as early as possible in your application
+
+// Essential globals
 global.Buffer = require('buffer').Buffer;
 global.process = require('process/browser');
 global.process.env = global.process.env || {};
+global.process.browser = true;
+global.process.version = 'v16.0.0'; // Mock Node.js version
+global.process.versions = { node: '16.0.0' };
 
 // Make sure crypto library works properly
 if (typeof global.crypto !== 'object') {
   global.crypto = require('crypto-browserify');
 }
 
-// Explicitly patch required Node.js modules
-if (typeof global.stream === 'undefined') {
-  global.stream = require('readable-stream');
-}
+// Network-related modules
+global.net = require('./net-shim');
+global.tls = require('./tls-shim');
+global.http = require('stream-http');
+global.https = require('https-browserify');
+global.dgram = require('./dgram-shim');
+global.dns = require('./dns-shim');
 
-if (typeof global.http === 'undefined') {
-  global.http = require('stream-http');
-}
+// File system and paths
+global.fs = require('./fs-shim');
+global.path = require('path-browserify');
 
-if (typeof global.https === 'undefined') {
-  global.https = require('https-browserify');
-}
+// Utilities
+global.util = require('util');
+global.assert = require('assert');
+global.stream = require('readable-stream');
+global.zlib = require('browserify-zlib');
+global.constants = require('constants-browserify');
+global.os = require('os-browserify');
+global.vm = require('vm-browserify');
+global.tty = require('tty-browserify');
+global.timers = require('timers-browserify');
+global.domain = require('domain-browser');
+
+// Web-related modules
+global.url = require('url');
+global.punycode = require('punycode');
+global.querystring = require('querystring-es3');
+global.string_decoder = require('string_decoder');
+
+// WebSockets
+global.ws = require('./ws-shim');
+
+// Console message to confirm initialization
+console.log('Node.js polyfills initialized');
