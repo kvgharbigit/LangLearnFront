8aff65e5ab515e2a07e7e7609d0b5912088a3b23
kvgharbigit <67672489+kvgharbigit@users.noreply.github.com>
2025-06-13 01:19:30 +1000

    stable post lemonfox STT

--- src/constants/debug.ts
+++ src/constants/debug.ts
@@ -7,6 +7,6 @@
 
 // Toggle to show/hide Developer Options in ProfileScreen
 // Set to true to show Developer Options tab, false to hide it
-export const DEBUG_TOOLS_TOGGLE = true;
+export const DEBUG_TOOLS_TOGGLE = false;
 
 // Other debug constants can be added here in the future
