9e9407eaa84137bfe7e42b5f52556db467009a61
kvgharbigit <67672489+kvgharbigit@users.noreply.github.com>
2025-07-16 17:22:48 +1000

    fixed backend link

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -12,10 +12,10 @@ import { estimateTokens } from '../constants/pricing';
 import userPreferences from './userPreferences';
 
 // Update this to your actual API URL
-//export const API_URL = 'https://language-tutor-984417336702.us-central1.run.app';
+export const API_URL = 'https://language-tutor-984417336702.us-central1.run.app';
 //const API_URL =  "http://172.20.10.2:8004" //iphone hotspot eduroam
 //export const API_URL ="http://10.0.0.116:8004" //desktop
-export const API_URL = "http://192.168.86.247:8004"
+//export const API_URL = "http://192.168.86.247:8004"
 
 
 
